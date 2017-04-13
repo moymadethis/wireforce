e303e740020049ab6e616007810f488c45dd749b
steve <steve@studioriffraff.com>
2017-04-13 15:42:49 +0100

    Updated site-nav css to match wordpress rather than using custom walkers etc

--- resources/templates/js/main.js
+++ resources/templates/js/main.js
@@ -7,9 +7,9 @@
  * Creates classes to enable responsive navigation.
  */
 
-$(function(){
-	if ($(".site-nav__list")) {
-		var navigation = responsiveNav(".site-nav__list", {
+jQuery(function($){
+	if ($(".site-nav ul")) {
+		var navigation = responsiveNav(".site-nav ul", {
 			customToggle: "#site-nav__toggle",
 			open: function(){
 				$("#site-nav__toggle").addClass('open');
@@ -29,6 +29,8 @@ $(function(){
 
 /**
  * Styling selects and file inputs.
- */ 
-
-$("select, input[type='file'], input[type='checkbox'], input[type='radio']").uniform({selectAutoWidth: false, fileButtonClass: 'btn'});
+ */
+ 
+jQuery(function($){
+	$("select, input[type='file'], input[type='checkbox'], input[type='radio']").uniform({selectAutoWidth: false, fileButtonClass: 'btn'});
+});
